2d240761a1b571149b9824f5f3e7b701b495dd04
nikinet <netesnik02@gmail.com>
2025-11-11 21:56:41 +0000

    feat(types): add `Next` type as alias for `NextFunction`

--- src/shared/types/request.types.ts
+++ src/shared/types/request.types.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { Request, Response } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
 export type Req<Params = any, Body = any, Query = any> = Request<Params, any, Body, Query>;
 export type ReqWithBody<Body> = Req<any, Body>;
 
 export type Res<Body = any, Locals extends Record<string, any> = Record<string, any>> = Response<Body, Locals>;
+
+export type Next = NextFunction;
 
 export type IdParams = { id: string };
