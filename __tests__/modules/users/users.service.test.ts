d13ae30a22287f14510d32977a6e0ae5f340f357
nikinet <netesnik02@gmail.com>
2025-11-11 22:07:47 +0000

    refactor(users.service): use new types

--- __tests__/modules/users/users.service.test.ts
+++ __tests__/modules/users/users.service.test.ts
@@ -2,9 +2,9 @@ import { beforeEach, describe, expect, jest, test } from '@jest/globals';
 import { UsersService } from '@src/modules/users/users.service.js';
 import { UsersRepository } from '@src/modules/users/users.repository.js';
 import { existingId, fakeUser, nonExistentId } from './fakes/users.fakes.js';
-import { USER_NOT_FOUND } from '@src/shared/errors/users.errors.js';
 import type { CreateUserDto } from '@src/modules/users/dto/create-user.dto.js';
 import type { UpdateUserInfoDto } from '@src/modules/users/dto/update-user-info.dto.js';
+import { UserNotFoundError } from '@src/shared/errors/not-found.error.js';
 
 jest.mock('@src/modules/users/users.repository.js');
 
@@ -39,12 +39,12 @@ describe('usersService.getUserById', () => {
     expect(promise).resolves.toEqual(user);
   });
 
-  test(`should throw an error with message "${USER_NOT_FOUND}" when given a non-existent id`, () => {
+  test('should throw UserNotFoundError when given a non-existent id', () => {
     mockUsersRepository.findById.mockResolvedValueOnce(null);
     const promise = usersService.getUserById(nonExistentId);
 
     expect(mockUsersRepository.findById).toHaveBeenCalledWith(nonExistentId);
-    expect(promise).rejects.toThrow(USER_NOT_FOUND);
+    expect(promise).rejects.toBeInstanceOf(UserNotFoundError);
   });
 });
 
@@ -60,14 +60,14 @@ describe('usersService.updateUserInfo', () => {
     expect(promise).resolves.toEqual(user);
   });
 
-  test(`should throw an error with message "${USER_NOT_FOUND}" when given a non-existent id`, () => {
+  test('should throw UserNotFoundError when given a non-existent id', () => {
     const fakeDto: UpdateUserInfoDto = { age: 25 };
 
     mockUsersRepository.update.mockResolvedValueOnce(null);
     const promise = usersService.updateUserInfo(nonExistentId, fakeDto);
 
     expect(mockUsersRepository.update).toHaveBeenCalledWith(nonExistentId, fakeDto);
-    expect(promise).rejects.toThrow(USER_NOT_FOUND);
+    expect(promise).rejects.toBeInstanceOf(UserNotFoundError);
   });
 });
 
@@ -80,12 +80,12 @@ describe('usersService.deleteUser', () => {
     expect(promise).resolves.toBeUndefined();
   });
 
-  test(`should throw an error with message "${USER_NOT_FOUND}" when given a non-existent id`, () => {
+  test('should throw UserNotFoundError when given a non-existent id', () => {
     mockUsersRepository.delete.mockResolvedValueOnce(0);
     const promise = usersService.deleteUser(nonExistentId);
 
     expect(mockUsersRepository.delete).toHaveBeenCalledWith(nonExistentId);
-    expect(promise).rejects.toThrow(USER_NOT_FOUND);
+    expect(promise).rejects.toBeInstanceOf(UserNotFoundError);
   });
 });
 
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { USER_NOT_FOUND } from '@src/shared/errors/users.errors.js';
+import { UserNotFoundError } from '@src/shared/errors/not-found.error.js';
 import type { CreateUserDto } from './dto/create-user.dto.js';
 import type { UpdateUserInfoDto } from './dto/update-user-info.dto.js';
 import type { User } from './entities/user.entity.js';
@@ -14,19 +14,19 @@ class UsersService {
 
   async getUserById(id: number): Promise<User> {
     const user = await this.usersRepository.findById(id);
-    if (!user) throw new Error(USER_NOT_FOUND);
+    if (!user) throw new UserNotFoundError();
     return user;
   }
 
   async updateUserInfo(id: number, userInfo: UpdateUserInfoDto): Promise<User> {
     const updatedUser = await this.usersRepository.update(id, userInfo);
-    if (!updatedUser) throw new Error(USER_NOT_FOUND);
+    if (!updatedUser) throw new UserNotFoundError();
     return updatedUser;
   }
 
   async deleteUser(id: number): Promise<void> {
     const count = await this.usersRepository.delete(id);
-    if (!count) throw new Error(USER_NOT_FOUND);
+    if (!count) throw new UserNotFoundError();
   }
 }
 
