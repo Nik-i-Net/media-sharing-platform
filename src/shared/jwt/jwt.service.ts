9ecb6d52004ec3e95a448648c7f70e1908673f07
nikinet <netesnik02@gmail.com>
2025-11-14 18:19:08 +0000

    refactor(jwt): merge jwt.service and jwt.utils

--- src/shared/jwt/jwt.service.ts
+++ src/shared/jwt/jwt.service.ts
@@ -1,5 +1,5 @@
-import { signAsync, verifyAsync } from './jwt.utils.js';
-import type { JwtPayload, SignOptions, VerifyOptions } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import type { JwtPayload, PrivateKey, PublicKey, SignOptions, VerifyOptions } from 'jsonwebtoken';
 import type { JwtConfig, JwtToken, TokenType } from '../types/jwt.types.js';
 
 class JwtService {
@@ -21,6 +21,36 @@ class JwtService {
     const payload = await verifyAsync(token, publicKey, options);
     return payload;
   }
+}
+
+function signAsync(
+  payload: JwtPayload,
+  privateKey: PrivateKey,
+  options: SignOptions,
+): Promise<JwtToken> {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, privateKey, options, (err, token) => {
+      if (err) return reject(err);
+      if (!token) return reject('Failed to sign JWT: token is undefined');
+      resolve(token);
+    });
+  });
+}
+
+function verifyAsync(
+  token: JwtToken,
+  publicKey: PublicKey,
+  options: VerifyOptions,
+): Promise<JwtPayload> {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, publicKey, options, (err, payload) => {
+      if (err) return reject(err);
+      if (!payload || typeof payload === 'string') {
+        return reject('Invalid payload');
+      }
+      resolve(payload);
+    });
+  });
 }
 
 export { JwtService };
