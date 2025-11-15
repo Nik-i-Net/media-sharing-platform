5c5e1877a0a6e54cc0501bdfb33957e8a46da492
nikinet <netesnik02@gmail.com>
2025-11-15 17:47:57 +0000

    chore(users.controller) remove explicit return types

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,24 +8,24 @@ import type { UsersService } from './users.service.js';
 class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  createUser = async (req: ReqWithBody<CreateUserDto>, res: Res<User>): Promise<void> => {
+  createUser = async (req: ReqWithBody<CreateUserDto>, res: Res<User>) => {
     const user = await this.usersService.createUser(req.body);
     res.status(StatusCodes.CREATED).json(user);
   };
 
-  getUserById = async (req: Req<IdParams>, res: Res<User>): Promise<void> => {
+  getUserById = async (req: Req<IdParams>, res: Res<User>) => {
     const id = Number(req.params.id);
     const user = await this.usersService.getUserById(id);
     res.json(user);
   };
 
-  updateUserInfo = async (req: Req<IdParams, UpdateUserInfoDto>, res: Res<User>): Promise<void> => {
+  updateUserInfo = async (req: Req<IdParams, UpdateUserInfoDto>, res: Res<User>) => {
     const id = Number(req.params.id);
     const updatedUser = await this.usersService.updateUserInfo(id, req.body);
     res.json(updatedUser);
   };
 
-  deleteUser = async (req: Req<IdParams>, res: Res<void>): Promise<void> => {
+  deleteUser = async (req: Req<IdParams>, res: Res<void>) => {
     const id = Number(req.params.id);
     await this.usersService.deleteUser(id);
     res.sendStatus(StatusCodes.NO_CONTENT);
