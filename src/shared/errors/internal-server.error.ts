2a07d19ac74f25d631a864eeb7716d766e100b77
Nik-i-Net <netesnik02@gmail.com>
2026-01-09 22:29:55 +0200

    refactor (remove shared 2): errors

--- src/shared/errors/internal-server.error.ts
+++
@@ -1,9 +0,0 @@
-import { ReasonPhrases, StatusCodes } from 'http-status-codes';
-import { HttpError } from './http.error.js';
-
-export class InternalServerError extends HttpError {
-  constructor(message?: string) {
-    super(message ?? ReasonPhrases.INTERNAL_SERVER_ERROR, StatusCodes.INTERNAL_SERVER_ERROR);
-  }
-}
-
