d1bec17e32bdbc1074a4eb118d0fae65394952b8
Nik-i-Net <netesnik02@gmail.com>
2026-01-13 05:54:57 +0200

    feat (users.controller): use `PublicUser` as return type

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,29 +1,29 @@
 import { StatusCodes } from 'http-status-codes';
-import type { CreateUserDto } from './dto/create-user.dto.js';
-import type { UpdateUserDto } from './dto/update-user-info.dto.js';
-import type { User } from './entities/user.entity.js';
-import type { Req, Res, ReqWithBody, IdParams } from '../../shared/types/request.types.js';
+import type { UpdateUserDto } from './dto/update-user.dto.js';
+import type { Req, Res } from '../../common/types/express.types.js';
 import type { UsersService } from './users.service.js';
+import { type PublicUser, toPublicUser } from './dto/public-user.js';
 
 class UsersController {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
-  createUser = async (req: ReqWithBody<CreateUserDto>, res: Res<User>) => {
-    const user = await this.usersService.createUser(req.body);
-    res.status(StatusCodes.CREATED).json(user);
-  };
+  // NOTE: Auth module's responsibility
+  // createUser = async (req: ReqWithBody<CreateUserDto>, res: Res<UserDto>) => {
+  //   const user = await this.usersService.createUser(req.body);
+  //   res.status(StatusCodes.CREATED).json(user);
+  // };
 
-  getUserById = async (req: Req<IdParams>, res: Res<User>) => {
+  getUserById = async (req: Req<{ id: string }>, res: Res<PublicUser>) => {
     const user = await this.usersService.getUserById(req.params.id);
-    res.json(user);
+    res.json(toPublicUser(user));
   };
 
-  updateUserInfo = async (req: Req<IdParams, UpdateUserDto>, res: Res<User>) => {
+  updateUser = async (req: Req<{ id: string }, UpdateUserDto>, res: Res<PublicUser>) => {
     const updatedUser = await this.usersService.updateUser(req.params.id, req.body);
-    res.json(updatedUser);
+    res.json(toPublicUser(updatedUser));
   };
 
-  deleteUser = async (req: Req<IdParams>, res: Res<void>) => {
+  deleteUser = async (req: Req<{ id: string }>, res: Res<void>) => {
     await this.usersService.deleteUser(req.params.id);
     res.sendStatus(StatusCodes.NO_CONTENT);
   };
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { UsersRepository } from './users.repository.js';
 import { UsersService } from './users.service.js';
 import { UsersController } from './users.controller.js';
-import db from '@src/shared/database/database.js';
+import db from '@core/database/knex-client.js';
 
 const usersRepository = new UsersRepository(db);
 const usersService = new UsersService(usersRepository);
@@ -10,13 +10,9 @@ const usersController = new UsersController(usersService);
 const usersRouter = Router();
 
 usersRouter
-  .route('/') //
-  .post(usersController.createUser);
-
-usersRouter
   .route('/:id')
   .get(usersController.getUserById)
-  .patch(usersController.updateUserInfo)
+  .patch(usersController.updateUser)
   .delete(usersController.deleteUser);
 
 export { usersRouter, usersService };
