2644a52768ede8c835ee44758f78a1f8c06b3670
Nik-i-Net <netesnik02@gmail.com>
2026-01-30 00:07:29 +0200

    refactor!: add separate directory for app configs

--- src/@config/knexfile.ts
+++ src/@config/knexfile.ts
@@ -1,11 +1,11 @@
-import { wrapIdentifier, postProcessResponse } from './knex-utils.js';
+import { wrapIdentifier, postProcessResponse } from '@shared/infrastructure/persistence/knex-utils.js';
 import type { Knex } from 'knex';
 
 if (!process.env.DB_URL) {
   throw new Error('[env] DB_URL is missing');
 }
 
-const config: Knex.Config = {
+const knexConfig: Knex.Config = {
   client: 'pg',
   connection: {
     connectionString: process.env.DB_URL,
@@ -18,4 +18,4 @@ const config: Knex.Config = {
   postProcessResponse,
 };
 
-export default config;
+export default knexConfig;
