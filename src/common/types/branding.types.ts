7d60725051df869ad0983d9ce6c923fc061fabe8
Nik-i-Net <netesnik02@gmail.com>
2026-01-30 00:27:21 +0200

    refactor!: remove custom brand types in favor of `zod` lib

--- src/common/types/branding.types.ts
+++
@@ -1,3 +0,0 @@
-// TODO: add eslint rule to enforce controllers' return type to include __publicBrand
-declare const __publicBrand: unique symbol;
-export type Public<T> = T & { readonly [__publicBrand]: true };
