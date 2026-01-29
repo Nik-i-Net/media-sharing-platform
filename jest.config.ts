a046d794a7680344a0fcf3930dfb5143f12a1a7c
Nik-i-Net <netesnik02@gmail.com>
2026-01-29 23:55:02 +0200

    chore: update configs, add dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   transform: {
     '^.+\\.ts$': ['ts-jest', { tsconfig: './tsconfig.test.json' }],
   },
-  testMatch: ['__tests__/**/*.test.ts'],
+  testMatch: ['tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
 };
 
