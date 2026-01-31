657757c5fbd0ec66f5ad66b2173f5984b5d7c4f4
Nik-i-Net <netesnik02@gmail.com>
2026-01-31 19:20:18 +0200

    fix: remove async parsing in `validateRequest` middleware

--- src/@shared/infrastructure/web/middlewares/validate-request.middleware.ts
+++ src/@shared/infrastructure/web/middlewares/validate-request.middleware.ts
@@ -1,44 +1,32 @@
-import type { RequestHandler } from 'express';
-import type { ZodError, ZodType } from 'zod';
-import type { Next, Req } from '../express.types.js';
 import { ValidationException } from '@shared/application/errors/validation.exception.js';
+import type { RequestHandler } from 'express';
+import type { ZodType } from 'zod';
+import type { Next, Req } from '../express.types.js';
 
-type ValidateRequestOptions<P = unknown, B = unknown, Q = unknown> = {
-  params?: ZodType<P>;
-  body?: ZodType<B>;
-  query?: ZodType<Q>;
+type ValidateRequestSchemas<Params, Body, Query> = {
+  params?: ZodType<Params>;
+  body?: ZodType<Body>;
+  query?: ZodType<Query>;
 };
 
-export function validateRequest<P = unknown, B = unknown, Q = unknown>(
-  schemas: ValidateRequestOptions<P, B, Q>,
-): RequestHandler<P, unknown, B, Q> {
-  return async (req: Req, _res, next: Next) => {
-    const [paramsErrors, bodyErrors, queryErrors] = await Promise.all([
-      schemas.params?.safeParseAsync(req.params).then((result) => {
-        if (!result.success) return result.error;
-        req.params = result.data;
-      }),
-
-      schemas.body?.safeParseAsync(req.body).then((result) => {
-        if (!result.success) return result.error;
-        req.body = result.data;
-      }),
-
-      schemas.query?.safeParseAsync(req.query).then((result) => {
-        if (!result.success) return result.error;
-        req.query = result.data;
-      }),
-    ]);
-
+export function validateRequest<Params, Body, Query>(
+  schemas: ValidateRequestSchemas<Params, Body, Query>,
+): RequestHandler<Params, unknown, Body, Query> {
+  return (req: Req, _res, next: Next) => {
     const errors: { message: string; path: string }[] = [];
-    function collectErrors(path: string, zodError: ZodError) {
-      zodError.issues.forEach((err) => {
-        errors.push({ message: err.message, path: [path, ...err.path].join('.') });
-      });
+
+    for (const prop of ['params', 'body', 'query'] as const) {
+      if (schemas[prop] === undefined) continue;
+
+      const parseResult = schemas[prop].safeParse(req[prop]);
+      if (parseResult.success) {
+        req[prop] = parseResult.data;
+      } else {
+        parseResult.error.issues.forEach((err) => {
+          errors.push({ message: err.message, path: [prop, ...err.path].join('.') });
+        });
+      }
     }
-    if (paramsErrors) collectErrors('params', paramsErrors);
-    if (bodyErrors) collectErrors('body', bodyErrors);
-    if (queryErrors) collectErrors('query', queryErrors);
 
     if (errors.length) {
       throw new ValidationException(errors);
@@ -47,23 +35,3 @@ export function validateRequest<P = unknown, B = unknown, Q = unknown>(
     next();
   };
 }
-
-//
-// import { z } from 'zod';
-// import { ValidationException } from '@shared/application/errors/validation.exception.js';
-//
-// const Id = z.object({ id: z.object({ uuid: z.uuidv4() }) });
-// const Age = z.object({ age: z.number() });
-// const Email = z.object({ email: z.email() });
-//
-// // const result = await req.safeParseAsync({ id: crypto.randomUUID() + 5 });
-// const handler = validateRequest({ params: Id, body: Age, query: Email });
-//
-// const params = { id: { id: 5 } };
-// const body = { age: '33' };
-// const query = { email: 'test' };
-// const req = { params, body, query } as unknown as Req<{ id: { uuid: string } }, { age: number }, { email: string }>;
-//
-// handler(req, {} as Res, {} as Next);
-//
-// // if (!result.success) throw new Error('Invalid id')
