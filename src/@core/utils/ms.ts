bf003e2829521d30140f085b2e0243afb8df90ee
Nik-i-Net <netesnik02@gmail.com>
2026-02-09 04:47:09 +0200

    feat: add core utils

---
+++ src/@core/utils/ms.ts
@@ -0,0 +1,19 @@
+import type { Duration, DurationUnit } from '@core/types';
+
+export function ms(duration: Duration): number {
+  const value = parseFloat(duration);
+  const unit = duration.at(-1) as DurationUnit;
+
+  switch (unit) {
+    case 's':
+      return value * 1000;
+    case 'm':
+      return value * 1000 * 60;
+    case 'h':
+      return value * 1000 * 60 * 60;
+    case 'd':
+      return value * 1000 * 60 * 60 * 24;
+    default:
+      throw new Error(`Unexpected unit: ${unit satisfies never}`);
+  }
+}
