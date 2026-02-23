f8a8132c4d2842ae007329d2aeeae6f823d5dbc7
Nik-i-Net <netesnik02@gmail.com>
2026-02-23 05:40:32 +0200

    chore: add scripts to manage db container

---
+++ scripts/db-connect.js
@@ -0,0 +1,18 @@
+import { spawn } from 'child_process';
+
+const { POSTGRES_USER, POSTGRES_DB } = process.env;
+const CONTAINER_NAME = 'pgsql';
+
+if (!POSTGRES_USER || !POSTGRES_DB) {
+  console.error('Error: POSTGRES_USER or POSTGRES_DB not found in .env');
+  process.exit(1);
+}
+
+// docker exec -it <name> psql -U <user> -d <db>
+const psql = spawn('docker', ['exec', '-it', CONTAINER_NAME, 'psql', '-U', POSTGRES_USER, '-d', POSTGRES_DB], {
+  stdio: 'inherit',
+});
+
+psql.on('exit', (code) => {
+  process.exit(code);
+});
