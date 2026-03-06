4e7c8b5fa6e953b9d08f4c45d99672de0d9b00d8
Nik-i-Net <netesnik02@gmail.com>
2026-03-06 16:33:36 +0200

    feat: update `blobs` table migration

--- migrations/002_add_blobs_and_media.ts
+++ migrations/002_add_blobs_and_media.ts
@@ -2,25 +2,26 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('blobs', (table) => {
-    table.uuid('id').primary();
+    table.increments('id').primary();
 
     table.string('storage_key', 255).notNullable().unique();
-    table.string('content_type', 255).notNullable();
-    table.integer('content_length').notNullable();
-    table.string('hash', 255).notNullable().unique();
+    table.string('hash', 255).notNullable();
     table.string('hash_algorithm', 255).notNullable();
+    table.string('mime_type', 255).notNullable();
+    table.integer('size').notNullable();
     table.enum('status', ['pending', 'ready'], { useNative: true, enumName: 'blob_status' }).defaultTo('pending');
 
     table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
+
+    table.unique(['hash', 'hash_algorithm']);
   });
 
   await knex.schema.createTable('media', (table) => {
     table.string('id', 12).primary(); // nanoid
     table.uuid('user_id').nullable();
-    table.string('blob_id').notNullable();
+    table.integer('blob_id').unsigned().notNullable();
 
     table.string('title', 50).nullable();
-    table.enum('status', ['pending', 'ready'], { useNative: true, enumName: 'media_status' }).defaultTo('pending');
 
     table.timestamp('expires_at', { useTz: true }).nullable();
     table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(knex.fn.now());
