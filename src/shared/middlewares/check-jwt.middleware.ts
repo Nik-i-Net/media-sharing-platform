75032345fc030ea940c44267379be21af6ca6935
Nik-i-Net <netesnik02@gmail.com>
2026-04-09 13:23:25 +0300

    feat: add `checkJwt` middleware

---
+++ src/shared/middlewares/check-jwt.middleware.ts
@@ -0,0 +1,37 @@
+import { ENV } from '@config/env.loader';
+import { TodoException } from '@shared/errors';
+import { StatusCodes } from 'http-status-codes';
+import { createRemoteJWKSet, jwtVerify } from 'jose';
+import type { NextFunction, Request, Response } from 'express';
+
+const JWKS = createRemoteJWKSet(new URL(`${ENV.JWT_ISSUER}/.well-known/jwks.json`), {
+  cooldownDuration: 60 * 60 * 1000, // 1 hour
+});
+
+export async function checkJwt(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  const [scheme, token] = authHeader?.split(' ') ?? [];
+
+  if (scheme !== 'Bearer' || !token) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Missing or invalid token' });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS, {
+      issuer: ENV.JWT_ISSUER,
+      audience: ENV.JWT_AUDIENCE,
+      algorithms: ['RS256'],
+    });
+
+    const userId = payload[`${ENV.JWT_AUDIENCE}/userId`];
+    if (!userId || typeof userId !== 'string') {
+      throw new TodoException('Invalid userId');
+    }
+    req.user = { id: userId };
+
+    next();
+  } catch (error) {
+    console.error('JWT Validation Error:', error);
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Token is invalid or expired' });
+  }
+}
