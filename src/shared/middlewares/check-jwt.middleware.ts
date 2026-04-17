cffc4ba748526a10a44499e355dd53a1a71b2558
Nik-i-Net <netesnik02@gmail.com>
2026-04-18 00:37:00 +0300

    fix: add `/` to `issuer` in `checkJwt` middleware

--- src/shared/middlewares/check-jwt.middleware.ts
+++ src/shared/middlewares/check-jwt.middleware.ts
@@ -18,7 +18,7 @@ export async function checkJwt(req: Request, res: Response, next: NextFunction) 
 
   try {
     const { payload } = await jwtVerify(token, JWKS, {
-      issuer: ENV.JWT_ISSUER,
+      issuer: ENV.JWT_ISSUER + '/',
       audience: ENV.JWT_AUDIENCE,
       algorithms: ['RS256'],
     });
