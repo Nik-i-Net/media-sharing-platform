c963f9baa1501ef6fd5f9a84984015ca6956d533
Nik-i-Net <netesnik02@gmail.com>
2026-04-26 08:45:55 +0300

    wip: user.ts

--- src/features/users/domain/user.ts
+++ src/features/users/domain/user.ts
@@ -1,42 +1,47 @@
+import type { Identity } from './identity';
+
 interface RegisterUserProps {
   id: string;
   email?: string | undefined;
   emailVerified?: boolean | undefined;
 }
 
-interface NewUserProps {
+interface RestoreUserProps {
   id: string;
   email: string | null;
   emailVerified: boolean;
+  totalStorageBytes: number;
   createdAt: Date;
   updatedAt: Date;
 }
 
 export class User {
   static register(props: RegisterUserProps) {
-    const user = new User({
-      id: props.id,
-      email: props.email ?? null,
-      emailVerified: props.emailVerified ?? false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-    return user;
+    const { id, email = null, emailVerified = null } = props;
+    const totalStorageBytes = 0;
+    const now = new Date();
+    return new User(id, email, emailVerified, totalStorageBytes, now, now);
   }
 
-  readonly id: string;
-  private _email: string | null;
-  private _emailVerified: boolean;
-  readonly createdAt: Date;
-  private _updatedAt: Date;
-
-  constructor(props: NewUserProps) {
-    this.id = props.id;
-    this._email = props.email;
-    this._emailVerified = props.emailVerified;
-    this.createdAt = props.createdAt;
-    this._updatedAt = props.updatedAt;
+  static restore(props: RestoreUserProps) {
+    return new User(
+      props.id,
+      props.email,
+      props.emailVerified,
+      props.totalStorageBytes,
+      props.createdAt,
+      props.updatedAt,
+    );
   }
+
+  protected constructor(
+    readonly id: string,
+    private _email: string | null,
+    private _emailVerified: boolean | null,
+    private _totalStorageBytes: number,
+    readonly createdAt: Date,
+    protected _updatedAt: Date,
+  ) {}
 
   get email() {
     return this._email;
@@ -47,18 +52,75 @@ export class User {
   get updatedAt() {
     return this._updatedAt;
   }
+  get totalStorageBytes() {
+    return this._totalStorageBytes;
+  }
 
   changeEmail(newEmail: string) {
     this._email = newEmail;
-    this.touch();
+    this._updatedAt = new Date();
   }
 
   verifyEmail() {
     this._emailVerified = true;
-    this.touch();
+    this._updatedAt = new Date();
+  }
+}
+
+interface RegisterUserWithIdentitiesProps extends RegisterUserProps {
+  identities: Identity[];
+}
+
+interface RestoreUserWithIdentitiesProps extends RestoreUserProps {
+  identities: Identity[];
+}
+
+export class UserWithIdentities extends User {
+  static register(props: RegisterUserWithIdentitiesProps) {
+    const { id, email = null, emailVerified = null, identities = [] } = props;
+    const totalStorageBytes = 0;
+    const now = new Date();
+    return new UserWithIdentities(
+      id,
+      email,
+      emailVerified,
+      totalStorageBytes,
+      now,
+      now,
+      identities,
+    );
   }
 
-  private touch() {
+  static restore(props: RestoreUserWithIdentitiesProps) {
+    return new UserWithIdentities(
+      props.id,
+      props.email,
+      props.emailVerified,
+      props.totalStorageBytes,
+      props.createdAt,
+      props.updatedAt,
+      props.identities,
+    );
+  }
+
+  constructor(
+    id: string,
+    email: string | null,
+    emailVerified: boolean | null,
+    totalStorageBytes: number,
+    createdAt: Date,
+    updatedAt: Date,
+    private _identities: Identity[],
+  ) {
+    super(id, email, emailVerified, totalStorageBytes, createdAt, updatedAt);
+  }
+
+  get identities() {
+    return this._identities;
+  }
+
+  addIdentity(identity: Identity) {
+    this._identities.push(identity);
     this._updatedAt = new Date();
   }
 }
