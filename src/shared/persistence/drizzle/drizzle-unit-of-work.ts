c4fe620f25d57675843eef77d91bb5a8a923f576
Nik-i-Net <netesnik02@gmail.com>
2026-04-28 20:19:15 +0300

    feat: implement `DrizzleUnitOfWork`

---
+++ src/shared/persistence/drizzle/drizzle-unit-of-work.ts
@@ -0,0 +1,24 @@
+import { DrizzleUsersRepository } from '@/features/users/repositories/drizzle-users.repository';
+import type { UnitOfWork, UnitOfWorkContext } from '@/shared/ports/unit-of-work';
+import type { DrizzleDB, DrizzleTransaction } from './client';
+
+export class DrizzleUnitOfWork implements UnitOfWork {
+  constructor(private readonly db: DrizzleDB) {}
+
+  async execute<T>(work: (ctx: DrizzleUnitOfWorkContext) => Promise<T>): Promise<T> {
+    return await this.db.transaction(async (trx) => {
+      const ctx = new DrizzleUnitOfWorkContext(trx);
+      return await work(ctx);
+    });
+  }
+}
+
+export class DrizzleUnitOfWorkContext implements UnitOfWorkContext {
+  private _usersRepository?: DrizzleUsersRepository;
+
+  constructor(private readonly trx: DrizzleTransaction) {}
+
+  get usersRepository(): DrizzleUsersRepository {
+    return (this._usersRepository ??= new DrizzleUsersRepository(this.trx));
+  }
+}
