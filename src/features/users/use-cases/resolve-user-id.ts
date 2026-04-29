b820c31b47ddbf4b7694f5aa8887c1658c63ab64
Nik-i-Net <netesnik02@gmail.com>
2026-04-30 01:57:24 +0300

    feat(users): reimplement authentication use case

---
+++ src/features/users/use-cases/resolve-user-id.ts
@@ -0,0 +1,67 @@
+import { User, type Identity } from '../domain/user';
+import { TodoError } from '@/shared/errors';
+import type { UsersRepository } from '../domain/users.repository';
+
+type Identities = [Identity, ...Identity[]];
+
+export interface ResolveUserIdCommand {
+  externalId: string;
+  email: string | null;
+  emailVerified: boolean;
+  identities: Identities;
+}
+
+export class ResolveUserIdUseCase {
+  constructor(private readonly usersRepo: UsersRepository) {}
+
+  async execute(cmd: ResolveUserIdCommand): Promise<string> {
+    const userByExternalId = await this.usersRepo.findByExternalId(cmd.externalId);
+    if (userByExternalId) {
+      await this.syncIdentities(userByExternalId, cmd.identities);
+      return userByExternalId.id;
+    }
+
+    if (cmd.email) {
+      const userByEmail = await this.usersRepo.findByEmail(cmd.email);
+      if (userByEmail) await this.resolveEmailConflict(userByEmail, cmd.emailVerified);
+    }
+
+    const newUser = User.register({
+      id: crypto.randomUUID(),
+      externalId: cmd.externalId,
+      email: cmd.email,
+      emailVerified: cmd.emailVerified,
+      identity: cmd.identities[0],
+    });
+    await this.usersRepo.save(newUser);
+
+    return newUser.id;
+  }
+
+  private async syncIdentities(user: User, identities: Identities) {
+    const lastUpdatedAt = user.updatedAt;
+    const includes = (arr: Identity[], value: Identity) => {
+      return arr.some((i) => i.provider === value.provider && i.userId === value.userId);
+    };
+
+    for (const i of user.identities) {
+      if (!includes(identities, i)) user.removeIdentity(i);
+    }
+
+    for (const i of identities) {
+      if (!includes(user.identities, i)) user.addIdentity(i);
+    }
+
+    if (user.updatedAt !== lastUpdatedAt) await this.usersRepo.save(user);
+  }
+
+  private async resolveEmailConflict(userByEmail: User, newEmailVerified: boolean) {
+    if (!userByEmail.emailVerified && newEmailVerified) {
+      userByEmail.removeEmail();
+      userByEmail.suspendAccount();
+      await this.usersRepo.save(userByEmail);
+    } else {
+      throw new TodoError('Email already in use. Login to link accounts');
+    }
+  }
+}
