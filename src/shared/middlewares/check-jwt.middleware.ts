fe874fa8c846f82a603da05ee0c31364b6d87474
Nik-i-Net <netesnik02@gmail.com>
2026-05-01 00:21:04 +0300

    feat(middleware): add option not to reject unauthorized request in `checkJwt`

--- src/shared/middlewares/check-jwt.middleware.ts
+++ src/shared/middlewares/check-jwt.middleware.ts
@@ -1,39 +1,45 @@
 import { createRemoteJWKSet, jwtVerify } from 'jose';
-import type { NextFunction, Request, Response } from 'express';
 import { ENV } from '@/config/env.loader';
-import { StatusCodes } from '../constants';
-import { TodoError } from '../errors';
+import { TodoError, UnauthorizedError } from '../errors';
+import { JOSEError } from 'jose/errors';
+import type { RequestHandler } from 'express';
 
 const JWKS = createRemoteJWKSet(new URL(`${ENV.JWT_ISSUER}/.well-known/jwks.json`), {
   cooldownDuration: 60 * 60 * 1000, // 1 hour
 });
 
-export async function checkJwt(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.headers.authorization;
-  const [scheme, token] = authHeader?.split(' ') ?? [];
+export function checkJwt({ rejectRequest = true }): RequestHandler {
+  return async (req, _res, next) => {
+    const authHeader = req.headers.authorization;
+    const [scheme, token] = authHeader?.split(' ') ?? [];
 
-  if (scheme !== 'Bearer' || !token) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Missing or invalid token' });
-  }
-
-  try {
-    const { payload } = await jwtVerify(token, JWKS, {
-      issuer: ENV.JWT_ISSUER + '/',
-      audience: ENV.JWT_AUDIENCE,
-      algorithms: ['RS256'],
-    });
-
-    const userId = payload[`${ENV.JWT_AUDIENCE}/userId`];
-    if (!userId || typeof userId !== 'string') {
-      throw new TodoError('Invalid userId');
+    if (scheme !== 'Bearer' || !token) {
+      throw new UnauthorizedError('Missing or invalid token');
     }
-    req.user = { id: userId };
 
-    next();
-  } catch (error) {
-    console.error('JWT Validation Error:', error);
-    return res.status(StatusCodes.UNAUTHORIZED).json({ error: 'Token is invalid or expired' });
-  }
+    try {
+      const { payload } = await jwtVerify(token, JWKS, {
+        issuer: ENV.JWT_ISSUER + '/',
+        audience: ENV.JWT_AUDIENCE,
+        algorithms: ['RS256'],
+      });
+
+      const userId = payload[`${ENV.JWT_AUDIENCE}/userId`];
+      if (!userId || typeof userId !== 'string') {
+        throw new TodoError('Invalid userId');
+      }
+      req.user = { id: userId };
+
+      next();
+    } catch (err) {
+      if (!rejectRequest) next();
+
+      if (err instanceof JOSEError) {
+        throw new UnauthorizedError();
+      }
+      throw err;
+    }
+  };
 }
 
 declare module 'express-serve-static-core' {
