76fe592e0f7caadabb0853f82d39e858ce1f1d01
Nik-i-Net <netesnik02@gmail.com>
2026-05-10 17:23:45 +0300

    refactor: cleanup

--- src/features/collections/http/collection-summary.dto.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-import { CollectionDtoSchema } from './collection.dto';
-
-export const CollectionSummaryDtoSchema = CollectionDtoSchema.pick({
-  id: true,
-  title: true,
-});
-
-export type CollectionSummaryDto = z.infer<typeof CollectionSummaryDtoSchema>;
--- src/features/collections/http/create-collection.dto.ts
+++
@@ -1,9 +0,0 @@
-import { UserIdSchema } from '../../../shared/schemas/primitives.zod';
-import { z } from 'zod';
-
-export const CreateCollectionDtoSchema = z.object({
-  userId: UserIdSchema,
-  collectionName: z.string().nonempty(),
-});
-
-export type CreateCollectionDto = z.infer<typeof CreateCollectionDtoSchema>;
