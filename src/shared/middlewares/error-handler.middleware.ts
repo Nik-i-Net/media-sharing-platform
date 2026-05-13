c7331957fa51283dc784bdac344b0bcb9cefb5b6
Nik-i-Net <netesnik02@gmail.com>
2026-05-13 04:43:23 +0300

    feat: make global error handler to catch its own errors

--- src/shared/middlewares/error-handler.middleware.ts
+++ src/shared/middlewares/error-handler.middleware.ts
@@ -1,5 +1,4 @@
 import type { Request, Response, NextFunction } from 'express';
-import { StatusCodes } from '../constants';
 import { NotFoundError, UnauthorizedError, ValidationError } from '../errors';
 import { BaseError } from '../errors/base.error';
 import { z } from 'zod';
@@ -26,37 +25,32 @@ function errorEnvelope(err: ErrorObject): ResponseObject {
   });
 }
 
-function respondWithInternalServerError(res: Response<ResponseObject>) {
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
-    errorEnvelope({
-      message: 'Internal server error',
-      code: 'INTERNAL_SERVER_ERROR',
-    }),
-  );
-}
-
 export function errorHandler(
   err: unknown,
   req: Request,
   res: Response<ResponseObject>,
   _next: NextFunction,
 ) {
-  if (!(err instanceof BaseError)) {
-    console.error('UnknownError');
+  try {
+    if (!(err instanceof BaseError)) throw err;
+
+    if (
+      err instanceof NotFoundError || //
+      err instanceof UnauthorizedError
+    ) {
+      return res.status(err.httpStatusCode).json(errorEnvelope(err));
+    }
+    if (err instanceof ValidationError) {
+      return res.status(err.httpStatusCode).json(errorEnvelope({ ...err, details: err.issues }));
+    }
+
+    throw err;
+  } catch (err) {
+    console.error('Unhandled Error');
     console.error(`Route: ${req.method} ${req.url}`);
     console.error(err instanceof Error ? err.stack || err.message : err);
-    return respondWithInternalServerError(res);
+    res.status(500).json({
+      error: { message: 'Internal server error', code: 'INTERNAL_SERVER_ERROR' },
+    } as ResponseObject);
   }
-
-  if (
-    err instanceof NotFoundError || //
-    err instanceof UnauthorizedError
-  ) {
-    return res.status(err.httpStatusCode).json(errorEnvelope(err));
-  }
-  if (err instanceof ValidationError) {
-    return res.status(err.httpStatusCode).json(errorEnvelope({ ...err, details: err.issues }));
-  }
-
-  respondWithInternalServerError(res);
 }
