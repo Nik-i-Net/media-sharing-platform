92015ce98cb2f2d37988f79494099e7d9421c39c
Nik-i-Net <netesnik02@gmail.com>
2026-05-14 09:46:40 +0300

    feat(HashVO): add caching

--- src/features/uploads/domain/hash.value-object.ts
+++ src/features/uploads/domain/hash.value-object.ts
@@ -3,12 +3,15 @@ export class HashVO {
     return new HashVO(Buffer.from(hex, 'hex'));
   }
 
+  #hex?: string;
+  #base64?: string;
+
   constructor(readonly value: Buffer) {}
 
   get base64() {
-    return this.value.toString('base64');
+    return (this.#base64 ??= this.value.toString('base64'));
   }
   get hex() {
-    return this.value.toString('hex');
+    return (this.#hex ??= this.value.toString('hex'));
   }
 }
