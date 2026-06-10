a85a7fc3346c85faef87bcc61b6704afeed3ace0
Nik-i-Net <netesnik02@gmail.com>
2026-06-10 22:39:51 +0300

    chore: add docker-compose.prod.yml

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import { app } from './app/app';
 import { ENV } from './shared/env.loader';
 
 const port = Number(ENV.PORT);
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+});
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    process.exit(0);
+  });
 });
